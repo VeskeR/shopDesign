17ace0883a904869c69ee6779a109b662193748d
Andrey Bulat <andrii.bulat@gmail.com>
2016-03-16 01:28:17 +0200

    Made ripple scale to cover all available space

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
 	$('.ripple').on('mousedown', function (e) {
-		var ripple = $('<div class="ripple--active ripple--animate"></div>');
+		var ripple = $('<div class="ripple--active"></div>');
 		$(this).append(ripple);
 		var x = (e.pageX - $(this).offset().left);
 		var y = (e.pageY - $(this).offset().top);
 		ripple.css('top', y).css('left', x);
-		$(ripple).on('mouseout mouseup', function (e) {
+		var thisWidth = parseInt($(this).css('width'));
+		var thisHeight = parseInt($(this).css('height'));
+		var scale = Math.max(calcDistTo(0, 0), calcDistTo(0, thisHeight), calcDistTo(thisWidth, 0), calcDistTo(thisWidth, thisHeight));
+		function calcDistTo(x1, y1) {
+			return calcDist(x, y, x1, y1);
+		}
+		function calcDist (x0, y0, x1, y1) {
+			return Math.sqrt((x0 - x1)*(x0 - x1) + (y0 - y1)*(y0 - y1));
+		}
+		// Add 5% to ensure ripple covers all space
+		scale *= 1.05;
+		$(ripple).animate({ textIndent: scale }, {
+				step: function (now) {
+					$(this).css('-webkit-transform','scale(' + now + ')');
+				},
+				duration: 1000
+		});
+		$(ripple).add(window).on('mouseout mouseup', function (e) {
 			ripple.addClass('ripple--fade-out');
 			setTimeout(function() {
 				ripple.remove();
